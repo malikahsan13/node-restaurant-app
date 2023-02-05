a30d83e0254e10f62bb1b9a6201074a49209978c
MalikAhsan-PC <malikahsan13@gmail.com>
2023-02-05 12:43:33 +0500

    fixed jwt token and verification logic

--- routes/product_route.js
+++ routes/product_route.js
@@ -9,34 +9,38 @@ const router = express.Router()
 const SECRET_KEY = config.KEY_CONFIG.SECRET_KEY;
 
 
-router.use((req, res, next) => {
-    console.log("Product Middleware")
-    try{
-        const decoded = jwt.verify(req.headers.authorization, SECRET_KEY)
-        //console.log(decoded);
-        User_Group.findOne({where: {group_id: decoded.group_id}})   // get group id from session/jwt token
-        .then((user) => 
-            {
-                if(user.length!==0)
+const middlewareProduct = //() => 
+    // {
+        // router.use(
+            (req, res, next) => {
+        console.log("Product Middleware")
+        try{
+            const {user_data} = jwt.verify(req.headers.authorization, SECRET_KEY)
+            // console.log(user_data);
+            User_Group.findOne({where: {group_id: user_data.group_id}})   // get group id from session/jwt token
+            .then((user) => 
                 {
-                    Group_Privilege.findOne({where: {group_id: decoded.group_id, privilege_id: decoded.privilege_id}}) //get id's from session/jwt token
-                    .then((group) => { if(group.length!==0) next() })
-                    .catch((err) => res.send("You Dont Have Privilege Please Contact Admin"))
+                    if(user.length!==0)
+                    {
+                        Group_Privilege.findOne({where: {group_id: user_data.group_id, privilege_id: user_data.privilege_id}}) //get id's from session/jwt token
+                        .then((group) => { if(group.length!==0) next() })
+                        .catch((err) => res.send("You Dont Have Privilege Please Contact Admin"))
+                    }
                 }
-            }
-        )
-        .catch((err) => res.send("You Dont Have Group, Privilege Please Contact Admin") )
-        //next()
-    }
-    catch(err){
-        res.send(`Token is Expired ${err}`)
-    }
-})
+            )
+            .catch((err) => res.send("You Dont Have Group, Privilege Please Contact Admin") )
+            //next()
+        }
+        catch(err){
+            res.send(`Token is Expired ${err}`)
+        }
+    // })
+}
 
 router.get("/", getProducts)
 router.get("/:id", getProduct)
-router.post("/add", addProduct)
-router.put("/:id", updateProduct)
-router.delete("/:id", deleteProduct)
+router.post("/add",middlewareProduct, addProduct)
+router.put("/:id",middlewareProduct, updateProduct)
+router.delete("/:id",middlewareProduct, deleteProduct)
 
 module.exports = router;
